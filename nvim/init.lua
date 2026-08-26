4c130eeef143fa3aeb87f8870d8497c9a8692424
Wesley Ellis <wesley@opslevel.com>
2026-08-26 14:41:16 -0300

    big nvim dump, pin deps, use plenary not fzf, new colour scheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,60 +1,62 @@
 local vim = vim
+
+-- maybe time to find a new plugini manager? does nvim have one?
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
 -- git stuff
-Plug('tpope/vim-fugitive')
-Plug('shumphrey/fugitive-gitlab.vim')
-Plug('lewis6991/gitsigns.nvim')
-Plug('tpope/vim-rhubarb')
+Plug('tpope/vim-fugitive', { ['commit'] = '3b753cf8c6a4dcde6edee8827d464ba9b8c4a6f0' })
+Plug('shumphrey/fugitive-gitlab.vim', { ['commit'] = '3584cf7e607ba7822f991d407a8d780e05aae287' })
+Plug('lewis6991/gitsigns.nvim', { ['commit'] = 'dd3f588bacbeb041be6facf1742e42097f62165d' })
+Plug('tpope/vim-rhubarb', { ['commit'] = '5496d7c94581c4c9ad7430357449bb57fc59f501' })
 
---Plug('junegunn/fzf', { ['do'] = function()
---  vim.fn['fzf#install']()
---end })
---Plug('junegunn/fzf.vim')
+Plug('nvim-lua/plenary.nvim', { ['commit'] = '74b06c6c75e4eeb3108ec01852001636d85a932b' })
+Plug('nvim-telescope/telescope.nvim', { ['release'] = '0.2.2', ['commit'] = '7d324792b7943e4aa16ad007212e6acc6f9fe335' })
 
-Plug('nvim-lua/plenary.nvim')
-Plug('nvim-telescope/telescope.nvim', { ['branch'] = '0.1.x'})
+Plug('scrooloose/nerdtree', { ['commit'] = '690d061b591525890f1471c6675bcb5bdc8cdff9' })
+Plug('romgrk/barbar.nvim', { ['commit'] = 'a4bef5b4fc1f064f2f673172252028eae18191c9' })
 
-Plug('scrooloose/nerdtree')
-Plug('romgrk/barbar.nvim')
+--Plug('tanvirtin/monokai.nvim', { ['commit'] = 'b8bd44d5796503173627d7a1fc51f77ec3a08a63' })
+--Plug('EdenEast/nightfox.nvim', { ['commit'] = '26b61b1f856ec37cae3cb64f5690adb955f246a1' })
+--Plug('f4z3r/gruvbox-material.nvim', { ['commit'] = 'ee006c962780358b8f03eaa918a3139b7c6f0d81' })
+Plug('miikanissi/modus-themes.nvim', { ['commit'] = '56233f0986ef6c8d0eab97e924e87144e584d970' })
 
-Plug('tanvirtin/monokai.nvim')
-Plug('EdenEast/nightfox.nvim')
-
-Plug('tpope/vim-rails')
+-- adds :A for switching between rails file and test
+Plug('tpope/vim-rails', { ['commit'] = 'b0a5c76f86ea214ade36ab0b811e730c3f0add67' })
 
 Plug('nvim-treesitter/nvim-treesitter', {
 	['do'] = function()
 		vim.cmd('TSUpdate')
 	end,
-        -- pin to master for neovim 0.11, change to main after upgrade
-	['branch'] = 'master'
+	['branch'] = 'main',
+	['commit'] = '4916d6592ede8c07973490d9322f187e07dfefac'
 })
-Plug 'nvim-treesitter/nvim-treesitter-textobjects'
-Plug 'nvim-treesitter/nvim-treesitter-context'
---Plug 'p00f/nvim-ts-rainbow'
+-- some cool selection thing I totally forgot about
+Plug('nvim-treesitter/nvim-treesitter-textobjects', { ['commit'] = '851e865342e5a4cb1ae23d31caf6e991e1c99f1e' })
 
-Plug('github/copilot.vim')
+Plug('github/copilot.vim', { ['commit'] = 'a12fd5672110c8aa7e3c8419e28c96943ca179be' })
 
-Plug('neovim/nvim-lspconfig')
+Plug('neovim/nvim-lspconfig', { ['commit'] = '246572944c2a1e2a646c2e7609ff619b0fe74c18' })
 
-Plug('lukas-reineke/indent-blankline.nvim')
+-- add visual ident lines, like in vscode
+Plug('lukas-reineke/indent-blankline.nvim', { ['commit'] = 'd28a3f70721c79e3c5f6693057ae929f3d9c0a03' })
 
 vim.call('plug#end')
 
 ---------------------------
 
+vim.cmd([[colorscheme modus]]) -- modus_operandi, modus_vivendi
+
 vim.wo.number = true
 
-vim.cmd("colorscheme nightfox")
+--require('gruvbox-material').setup({
+--  contrast = 'hard',
+--  italics = true,
+--})
+--vim.cmd.colorscheme('gruvbox-material')
 
 require('gitsigns').setup()
-
---vim.env.FZF_DEFAULT_COMMAND = 'rg --files --hidden'
---vim.keymap.set("n", "<C-p>", ":Files<CR>")
---vim.keymap.set("n", "<C-l>", ":Buffers<CR>")
 
 local builtin = require('telescope.builtin')
 vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = 'Telescope find files' })
@@ -79,7 +81,7 @@ require'barbar'.setup {
   },
 }
 
-require'nvim-treesitter.configs'.setup {
+require'nvim-treesitter'.setup {
   ensure_installed = {"python", "vimdoc", "luadoc", "ruby", "go", "graphql"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = {}, -- List of parsers to ignore installing
@@ -104,6 +106,7 @@ vim.lsp.config('ruby_lsp', {
   cmd_env = {
     -- Ignore bad sorbet config
     RUBY_LSP_BYPASS_TYPECHECKER = 'true',
+    DISABLE_SPRING = "1",
   },
   init_options = {
     addonSettings = {
@@ -115,15 +118,11 @@ vim.lsp.config('ruby_lsp', {
 })
 vim.lsp.enable('ruby_lsp')
 
+-- make %% expand to current file's dir
 vim.cmd("cnoremap <expr> %% getcmdtype() == ':' ? expand('%:h').'/' : '%%'")
--- cno = command mode
--- <expr> = when set this is an "expression" that will look for the next "token" (%% in this case) and replace it with the
--- rest of the expression
--- maybe this is what I want?
---vim.keymap.set("cnoremap", "<expr> $$", "expand('%:h').'/'")
 
 
-
+-- indent blankline
 require("ibl").setup()
 
 local map = vim.api.nvim_set_keymap
@@ -153,9 +152,10 @@ vim.g.no_ruby_maps = true
 --vim.cmd("set clipboard+=unnamedplus")
 
 -- enable diagnostics in virtual lines, instead of end of line
--- vim.diagnostic.config({
-  -- virtual_lines = true
--- })
+vim.diagnostic.config({
+  virtual_text = true,
+--  virtual_lines = true
+})
 
 
 -- default indent of 2
@@ -164,10 +164,3 @@ vim.o.shiftwidth = 2
 vim.o.expandtab = true
 -- treat tab as 2 spaces
 vim.o.softtabstop = 2
-
--- this is the number of spaces a <Tab> counts for while performing editing operations
--- and for what it looks like. Leaveing it ot 8 for now so you can see tabs
---vim.o.tabstop = 2
-
--- be smart about indent for c like languages?
--- vim.o.smartindent = true
